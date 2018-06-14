34e2412a411995b651024f9a923a47d9a9d37db5
tsuji.d <dram.dt.shonan@gmail.com>
2018-06-14 23:00:25 +0900

    auto-commit

---
+++ abc/abc039/B.java
@@ -0,0 +1,35 @@
+package abc039;
+import java.util.Scanner;
+
+public class B {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+//		char[] s = in.nextLine().toCharArray();
+		char[] s = "12".toCharArray();
+		int len = s.length;
+
+		int[][] dp = new int[len+1][2];
+		dp[0][0] = 1;
+
+		for (int i = 0; i < len; i++) {
+			// 確定させようとしている桁の数
+			int D = s[i]-'0';
+
+			// j -> 0：未満でない、1：未満
+			for (int j = 0; j < 2; j++) {
+				
+				// インクリメントで参照する桁の数。
+				// すでにN未満が確定していれば常に9まで参照可能
+				// 未確定の場合は、s[i]まで参照可能。
+				for (int d = 0; d <= (j == 1 ? 9 : D); d++) {
+					dp[i+1][j | (d < D ? 1 : 0)] += dp[i][j];
+				}
+			}
+		}
+
+		System.out.println(dp[len][0]+dp[len][1]);
+	}
+
+}
