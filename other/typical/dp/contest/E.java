0dfafdbd0b1760fc8af162c5bc79e4868938f923
tsuji.d <dram.dt.shonan@gmail.com>
2018-06-15 23:00:21 +0900

    auto-commit

---
+++ other/typical/dp/contest/E.java
@@ -0,0 +1,112 @@
+package typical.dp.contest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
+
+public class E {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		long[][][] memo;
+		char[] s;
+		int d;
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			d = in.nextInt();
+			s = in.nextString().toCharArray();
+			memo = new long[s.length+1][d+1][2];
+
+			out.println(dfs(0, 0, 1) - 1);
+
+		}
+
+		long dfs(int i, int sum, int tight) {
+			if (memo[i][sum][tight] > 0) {
+				return memo[i][sum][tight];
+			}
+			if (s.length == i) return sum % d == 0 ? 1 : 0;
+
+			long ret = 0;
+			int D = s[i]-'0';
+
+			for (int e = 0; e <= (tight == 1 ? D : 9); e++) {
+				ret += dfs(i+1, (sum+e)%d, tight == 1 && D == e ? 1 : 0) % MOD;
+			}
+
+			return memo[i][sum][tight] = ret % MOD;
+		}
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
