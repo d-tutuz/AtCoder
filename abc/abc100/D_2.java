10c90476b7c2ede7d4b76da75732ad15450df17a
tsuji.d <dram.dt.shonan@gmail.com>
2018-06-17 08:00:24 +0900

    auto-commit

--- abc/abc100/D_2.java
+++ abc/abc100/D_2.java
@@ -1,5 +1,118 @@
 package abc100;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class D_2 {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int n = in.nextInt(), m = in.nextInt();
+			long[][] num = new long[n][8];
+			for (int i = 0; i < n; i++) {
+				long x = in.nextLong();
+				long y = in.nextLong();
+				long z = in.nextLong();
+				num[i] = new long[]{x+y+z, x+y-z, x-y+z, x-y-z, -x+y+z, -x+y-z, -x-y+z, -x-y-z};
+			}
+			long ans = -INF;
+
+			for (int i = 0; i < 8; i++) {
+				long tmp = 0;
+				List<Long> tmpList = new ArrayList<>();
+				for (int j = 0; j < n; j++) {
+					tmpList.add(num[j][i]);
+				}
+
+				Collections.sort(tmpList, Comparator.reverseOrder());
+				for (int j = 0; j < m; j++) {
+					tmp += tmpList.get(j);
+				}
+
+				ans = Math.max(ans, tmp);
+			}
+
+			out.println(ans);
+
+		}
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
 
 }
