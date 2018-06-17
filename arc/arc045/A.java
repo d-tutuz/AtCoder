dce7b3b2cc875bf1d45b8ba9e9bbfdff7affe4ee
tsuji.d <dram.dt.shonan@gmail.com>
2018-06-18 08:00:26 +0900

    auto-commit

---
+++ arc/arc045/A.java
@@ -0,0 +1,15 @@
+package arc045;
+
+import java.util.Scanner;
+
+
+public class A {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		String[] str = in.nextLine().split(" ");
+		System.out.println(str);
+	}
+
+}
