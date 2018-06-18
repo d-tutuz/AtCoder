27521520be7faf30069b811834114b7ae96e0b52
tsuji.d <dram.dt.shonan@gmail.com>
2018-06-18 23:00:23 +0900

    auto-commit

--- arc/arc045/A.java
+++ arc/arc045/A.java
@@ -9,7 +9,26 @@ public class A {
 
 		Scanner in = new Scanner(System.in);
 		String[] str = in.nextLine().split(" ");
-		System.out.println(str);
+		int len = str.length;
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < len; i++) {
+			char a = str[i].charAt(0);
+			char out = 0;
+			if (a == 'L') {
+				out = '<';
+			} else if (a == 'R') {
+				out = '>';
+			} else if (a == 'A') {
+				out = 'A';
+			}
+			if (i > 0) {
+				sb.append(" ");
+			}
+			sb.append(out);
+		}
+
+		System.out.println(sb.toString());
 	}
 
 }
