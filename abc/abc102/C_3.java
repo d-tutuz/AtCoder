cbf6263f01afb7ef6fafe9bed1463950fbd00987
tsuji.d <dram.dt.shonan@gmail.com>
2018-07-03 08:00:28 +0900

    auto-commit

---
+++ abc/abc102/C_3.java
@@ -0,0 +1,127 @@
+package abc102;
+
+import static java.lang.Math.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
+
+public class C_3 {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 50;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		long[] a;
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int n = in.nextInt();
+			a = new long[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = in.nextLong() - (i+1);
+			}
+
+			long left = -(long)pow(10, 12);
+			long right = (long)pow(10, 12);
+
+			for (int i = 0; i < 1000; i++) {
+				long c1 = (left * 2L + right) / 3;
+				long c2 = (left + right * 2L) / 3;
+
+				if (func(c1) < func(c2)) {
+					right = c2;
+				} else {
+					left = c1;
+				}
+			}
+
+			long ans = LINF;
+
+			for (long b = left; b <= right; b++) {
+				ans = min(ans, func(b));
+			}
+
+			out.println(ans);
+
+		}
+
+		long func(long b) {
+			long ret = 0;
+			for (long l : a) {
+				ret += Math.abs(l - b);
+			}
+			return ret;
+		}
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
--- abc/abc102/Main.java
+++ abc/abc102/Main.java
@@ -1,128 +1,17 @@
 package abc102;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Map;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		InputStream inputStream = System.in;
-		OutputStream outputStream = System.out;
-		InputReader in = new InputReader(inputStream);
-		PrintWriter out = new PrintWriter(outputStream);
-		TaskX solver = new TaskX();
-		solver.solve(1, in, out);
-		out.close();
-	}
+	public static void main(String[] args) {
+		double a = Math.pow(10, 12) * 2;
 
-	static int INF = 1 << 30;
-	static int MOD = 1000000007;
-	static int[] mh4 = { 0, -1, 1, 0 };
-	static int[] mw4 = { -1, 0, 0, 1 };
-	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
-	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-	static class TaskX {
-
-		long[] a;
-		Map<Double, Double> memo = new HashMap<>();
-		public void solve(int testNumber, InputReader in, PrintWriter out) {
-
-			int n = in.nextInt();
-			a = new long[n];
-			for (int i = 0; i < n; i++) {
-				a[i] = in.nextLong()-(i+1);
-			}
-
-			double ans = Long.MAX_VALUE/2;
-			ans = ternarySearch(-INF, INF);
-			out.println(ans);
-
+		for (int i = 0; i < 1000; i++) {
+			a *= 0.667;
 		}
 
-		double f(double b) {
-
-			if (memo.containsKey(b)) {
-				return memo.get(b);
-			}
-
-			double ret = 0;
-			for (long l : a) {
-				ret += Math.abs(l - b);
-			}
-			memo.put(b, ret);
-			return ret;
-		}
-
-		double ternarySearch(double l, double r) {
-			for (int i = 0; i < 200; i++) {
-				double a = (l + l + r) / 3;
-				double b = (l + r + r) / 3;
-				if (f(a) > f(b))
-					l = a;
-				else
-					r = b;
-			}
-			return l;
-		}
+		System.out.println(a);
+		System.out.println(Long.MAX_VALUE);
 
 	}
-
-	static class InputReader {
-		BufferedReader in;
-		StringTokenizer tok;
-
-		public String nextString() {
-			while (!tok.hasMoreTokens()) {
-				try {
-					tok = new StringTokenizer(in.readLine(), " ");
-				} catch (IOException e) {
-					throw new InputMismatchException();
-				}
-			}
-			return tok.nextToken();
-		}
-
-		public int nextInt() {
-			return Integer.parseInt(nextString());
-		}
-
-		public long nextLong() {
-			return Long.parseLong(nextString());
-		}
-
-		public double nextDouble() {
-			return Double.parseDouble(nextString());
-		}
-
-		public int[] nextIntArray(int n) {
-			int[] res = new int[n];
-			for (int i = 0; i < n; i++) {
-				res[i] = nextInt();
-			}
-			return res;
-		}
-
-		public long[] nextLongArray(int n) {
-			long[] res = new long[n];
-			for (int i = 0; i < n; i++) {
-				res[i] = nextLong();
-			}
-			return res;
-		}
-
-		public InputReader(InputStream inputStream) {
-			in = new BufferedReader(new InputStreamReader(inputStream));
-			tok = new StringTokenizer("");
-		}
-	}
-
 }
