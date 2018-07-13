e56f5dbc7a89e98a3091c46bfd6d7e7e46db2747
tsuji.d <dram.dt.shonan@gmail.com>
2018-07-14 08:00:28 +0900

    auto-commit

---
+++ abc/abc019/D.java
@@ -0,0 +1,108 @@
+package abc019;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class D {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		@SuppressWarnings("unchecked")
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int n = in.nextInt();
+			List<P>[] g = new ArrayList[n];
+			g = Stream.generate(ArrayList::new).limit(n).toArray(List[]::new);
+
+		}
+
+		class P {
+			int to;
+			long weight;
+
+			public P(int to, long weight) {
+				super();
+				this.to = to;
+				this.weight = weight;
+			}
+		}
+
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
