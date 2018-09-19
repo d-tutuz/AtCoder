834ed252956ff4cdd87baca7926b748876869879
tsuji.d <dram.dt.shonan@gmail.com>
2018-09-19 23:00:41 +0900

    auto-commit

---
+++ abc/abc054/D_4.java
@@ -0,0 +1,178 @@
+package abc054;
+
+import static java.lang.Math.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
+
+public class D_4 {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		static int maxa = 400, maxb = 400;
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int n = in.nextInt(), ma = in.nextInt(), mb = in.nextInt();
+			long[][][] dp = new long[n+1][maxa+100][maxb+100];
+			fill(dp, INF);
+
+			dp[0][0][0] = 0;
+
+			int[] a = new int[n];
+			int[] b = new int[n];
+			int[] c = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = in.nextInt();
+				b[i] = in.nextInt();
+				c[i] = in.nextInt();
+			}
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < maxa; j++) {
+					for (int k = 0; k < maxb; k++) {
+						dp[i+1][j+a[i]][k+b[i]] = min(dp[i+1][j+a[i]][k+b[i]], dp[i][j][k] + c[i]);
+						dp[i+1][j][k] = min(dp[i+1][j][k], dp[i][j][k]);
+					}
+				}
+			}
+
+			long ans = INF;
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= maxa; j++) {
+					for (int k = 1; k <= maxb; k++) {
+						if (k * ma == j * mb) {
+							ans = min(ans, dp[i][j][k]);
+						}
+					}
+				}
+			}
+
+			out.println(ans == INF ? -1 : ans);
+
+		}
+	}
+
+	static void fill(long[][][] dp, long v) {
+		for (int i = 0; i < dp.length; i++) {
+			for (int j = 0; j < dp[0].length; j++) {
+				for (int k = 0; k < dp[0][0].length; k++) {
+					dp[i][j][k] = v;
+				}
+			}
+		}
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArrayDec(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt() - 1;
+			}
+			return res;
+		}
+
+		public int[] nextIntArray1Index(int n) {
+			int[] res = new int[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArrayDec(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong() - 1;
+			}
+			return res;
+		}
+
+		public long[] nextLongArray1Index(int n) {
+			long[] res = new long[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextLong();
+			}
+			return res;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] res = new double[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextDouble();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
