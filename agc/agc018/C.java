1097399473cbaf9cdb3db92683107a35897a6512
tsuji.d <dram.dt.shonan@gmail.com>
2018-09-27 08:00:57 +0900

    auto-commit

---
+++ agc/agc018/C.java
@@ -0,0 +1,176 @@
+package agc018;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class C {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int x = in.nextInt(), y = in.nextInt(), z = in.nextInt();
+			int u = x + y + z;
+
+			int[] cnt = {x, y, z};
+			PriorityQueue<P> pq = new PriorityQueue<>();
+
+			for (int i = 0; i < u; i++) {
+				long a = in.nextLong();
+				long b = in.nextLong();
+				long c = in.nextLong();
+				pq.add(new P(a, 0));
+				pq.add(new P(b, 1));
+				pq.add(new P(c, 2));
+			}
+
+			long sum = 0;
+			while (!pq.isEmpty()) {
+
+				P p = pq.remove();
+				if (cnt[p.idx] == 0) {
+					continue;
+				}
+				sum += p.n;
+				cnt[p.idx]--;
+			}
+
+			out.println(sum);
+
+		}
+	}
+
+	static class P implements Comparable<P> {
+		long n;
+		int idx;
+		public P(long n, int idx) {
+			super();
+			this.n = n;
+			this.idx = idx;
+		}
+		@Override
+		public String toString() {
+			return "P [n=" + n + ", i=" + idx + "]";
+		}
+		@Override
+		public int compareTo(P o) {
+			return Long.compare(o.n, this.n);
+		}
+
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArrayDec(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt() - 1;
+			}
+			return res;
+		}
+
+		public int[] nextIntArray1Index(int n) {
+			int[] res = new int[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArrayDec(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong() - 1;
+			}
+			return res;
+		}
+
+		public long[] nextLongArray1Index(int n) {
+			long[] res = new long[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextLong();
+			}
+			return res;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] res = new double[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextDouble();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
