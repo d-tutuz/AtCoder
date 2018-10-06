70859317dba9c104ae0ae8343fc85d4851595ce0
tsuji.d <dram.dt.shonan@gmail.com>
2018-10-07 08:01:17 +0900

    auto-commit

---
+++ abc/abc112/C_2.java
@@ -0,0 +1,180 @@
+package abc112;
+
+import static java.lang.Math.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
+
+public class C_2 {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		int n;
+		int[] xi, yi, hi;
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			n = in.nextInt();
+			xi = new int[n];
+			yi = new int[n];
+			hi = new int[n];
+
+			for (int i = 0; i < n; i++) {
+				xi[i] = in.nextInt();
+				yi[i] = in.nextInt();
+				hi[i] = in.nextInt();
+			}
+
+			for (int cx = 0; cx <= 100; cx++) {
+				for (int cy = 0; cy <= 100; cy++) {
+					int h = -1;
+					boolean ok = true;
+					for (int i = 0; i < n; i++) {
+						int tmp = hi[i] + abs(xi[i] - cx) + abs(yi[i] - cy);
+						if (hi[i] > 0) {
+							if (h == -1) {
+								h = tmp;
+							} else {
+								if (h != tmp) {
+									ok = false;
+									break;
+								}
+							}
+						}
+					}
+
+					if (!ok) continue;
+
+					for (int i = 0; i < n; i++) {
+						if (hi[i] == 0) {
+							int dist = abs(xi[i] - cx) + abs(yi[i] - cy);
+							if (h > dist) {
+								ok = false;
+								break;
+							}
+						}
+					}
+
+					if (ok) {
+						out.printf("%d %d %d\n", cx, cy, h);
+						return;
+					}
+				}
+			}
+
+		}
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArrayDec(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt() - 1;
+			}
+			return res;
+		}
+
+		public int[] nextIntArray1Index(int n) {
+			int[] res = new int[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArrayDec(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong() - 1;
+			}
+			return res;
+		}
+
+		public long[] nextLongArray1Index(int n) {
+			long[] res = new long[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextLong();
+			}
+			return res;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] res = new double[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextDouble();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
