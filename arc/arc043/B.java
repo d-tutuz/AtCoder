963a7a2652dbe19f239c5dcbf99ce3164e87953c
tsuji.d <dram.dt.shonan@gmail.com>
2018-10-17 23:00:55 +0900

    auto-commit

---
+++ arc/arc043/B.java
@@ -0,0 +1,186 @@
+package arc043;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
+
+public class B {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+//		inputStream = new FileInputStream(new File("/workspace/Atcoder/arc/arc043/input_test.txt"));
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int n = in.nextInt();
+			long[] d = in.nextLongArray1Index(n);
+			Arrays.sort(d);
+
+			// 左端が l (1-indexed) となるときの条件を満たす場合の数 (l, i, r)
+			long[] dp = new long[n+1];
+			for (int i = 0; i < n+1; i++) {
+				int ll = lowerBound(d, d[i]/2);
+				int lr = upperBound(d, d[i]/2);
+				int r = lowerBound(d, d[i]*2);
+				dp[ll] += (n + 1 - r) * (lr - ll + 1) % MOD;
+				dp[ll] %= MOD;
+			}
+			for (int i = n; i > 0; i--) {
+				dp[i-1] += dp[i];
+			}
+
+			for (int i = 0; i < n; i++) {
+				dp[i+1] += dp[i];
+				dp[i+1] %= MOD;
+			}
+			long ans = 0;
+			for (int i = 1; i < n+1; i++) {
+				long l = d[i]*2;
+				int idx = lowerBound(d, l);
+				ans += idx <= n-1 ? (MOD + dp[n] - dp[idx-1]) % MOD : 0;
+				ans %= MOD;
+			}
+
+			out.println(ans % MOD);
+		}
+	}
+
+	public static int upperBound(long[] a, long obj) {
+		int l = 0, r = a.length - 1;
+		while (r - l >= 0) {
+			int c = (l + r) / 2;
+			if (a[c] <= obj) {
+				l = c + 1;
+			} else {
+				r = c - 1;
+			}
+		}
+		return l;
+	}
+
+	public static int lowerBound(long[] a, long obj) {
+		int l = 0, r = a.length - 1;
+		while (r - l >= 0) {
+			int c = (l + r) / 2;
+			if (obj <= a[c]) {
+				r = c - 1;
+			} else {
+				l = c + 1;
+			}
+		}
+		return l;
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArrayDec(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt() - 1;
+			}
+			return res;
+		}
+
+		public int[] nextIntArray1Index(int n) {
+			int[] res = new int[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArrayDec(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong() - 1;
+			}
+			return res;
+		}
+
+		public long[] nextLongArray1Index(int n) {
+			long[] res = new long[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextLong();
+			}
+			return res;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] res = new double[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextDouble();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
