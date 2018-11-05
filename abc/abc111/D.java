d7713f368d43462be79dfa56ed46887dbced9400
tsuji.d <dram.dt.shonan@gmail.com>
2018-11-05 23:01:15 +0900

    auto-commit

---
+++ abc/abc111/D.java
@@ -0,0 +1,194 @@
+package abc111;
+
+import static java.lang.Math.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class D {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int n = in.nextInt();
+			long[] x = new long[n], y = new long[n];
+			long[] u = new long[n], v = new long[n];
+
+			for (int i = 0; i < n; i++) {
+				x[i] = in.nextLong();
+				y[i] = in.nextLong();
+				u[i] = x[i] + y[i];
+				v[i] = x[i] - y[i];
+				if (abs(x[i] - y[i]) % 2 != abs(x[0] - y[0]) % 2) {
+					out.println(-1);
+					return;
+				}
+			}
+
+			List<Long> list = new ArrayList<>();
+			for (int i = 0; i <= 30; i++) {
+				list.add(1L << i);
+			}
+			if (abs(x[0] - y[0]) % 2 == 0) {
+				list.add(1L);
+			}
+			Collections.sort(list, Comparator.reverseOrder());
+
+			String[] ans = new String[n];
+			for (int i = 0; i < n; i++) {
+				StringBuilder sb = new StringBuilder();
+				for (int j = 0; j < list.size(); j++) {
+					long d = list.get(j);
+					if (u[i] >= 0 && v[i] >= 0) {
+						sb.append("R");
+						u[i] -= d;
+						v[i] -= d;
+					} else if (u[i] >= 0 && v[i] <= 0) {
+						sb.append("U");
+						u[i] -= d;
+						v[i] += d;
+					} else if (u[i] <= 0 && v[i] >= 0) {
+						sb.append("D");
+						u[i] += d;
+						v[i] -= d;
+					} else if (u[i] <= 0 && v[i] <= 0) {
+						sb.append("L");
+						u[i] += d;
+						v[i] += d;
+					}
+				}
+				ans[i] = sb.toString();
+			}
+
+			out.println(list.size());
+			for (int i = 0; i < list.size(); i++) {
+				if (i > 0) out.print(" ");
+				out.print(list.get(i));
+			}
+			out.print("\n");
+			for (String str : ans) {
+				out.println(str);
+			}
+
+		}
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArrayDec(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt() - 1;
+			}
+			return res;
+		}
+
+		public int[] nextIntArray1Index(int n) {
+			int[] res = new int[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArrayDec(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong() - 1;
+			}
+			return res;
+		}
+
+		public long[] nextLongArray1Index(int n) {
+			long[] res = new long[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextLong();
+			}
+			return res;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] res = new double[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextDouble();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
