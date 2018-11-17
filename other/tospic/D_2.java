1ba9bbdff35db42acd63f71e51b8b268a97e462b
tsuji.d <dram.dt.shonan@gmail.com>
2018-11-18 02:31:37 +0900

    auto-commit

---
+++ other/tospic/D_2.java
@@ -0,0 +1,166 @@
+package tospic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.StringTokenizer;
+
+public class D_2 {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+			int n = in.nextInt(), m = in.nextInt();
+			int[][] mat = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(mat[i], -1);
+			}
+
+			for (int i = 0; i < m; i++) {
+				int a = in.nextInt()-1, b = in.nextInt()-1, h = in.nextInt();
+				mat[a][b] = h;
+				mat[b][a] = h;
+			}
+
+			int[][] dp = new int[1 << n][n];
+
+//			for (int i = 0; i < 1 << n; i++) {
+//				Arrays.fill(dp[i], -INF);
+//			}
+//			for (int i = 0; i < n; i++) {
+//				dp[1<<i][0] = 0;
+//			}
+
+			for (int bit = 0; bit < 1 << n; bit++) {
+				for (int now = 0; now < n; now++) {
+					for (int to = 0; to < n; to++) {
+						if ((bit >> now & 1) == 1 && (bit >> to & 1) == 0 && mat[now][to] != -1) {
+							dp[bit | 1 << to][to] = Math.max(dp[bit | 1 << to][to], dp[bit][now] + mat[now][to]);
+						}
+					}
+				}
+			}
+
+			int max = 0;
+			for (int i = 0; i < n; i++) {
+				max = Math.max(max, dp[(1<<n)-1][i]);
+			}
+			out.println(max);
+
+		}
+	}
+
+	static class InputReader {
+		BufferedReader in;
+		StringTokenizer tok;
+
+		public String nextString() {
+			while (!tok.hasMoreTokens()) {
+				try {
+					tok = new StringTokenizer(in.readLine(), " ");
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+			}
+			return tok.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(nextString());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public int[] nextIntArray(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArrayDec(int n) {
+			int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt() - 1;
+			}
+			return res;
+		}
+
+		public int[] nextIntArray1Index(int n) {
+			int[] res = new int[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextInt();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArrayDec(int n) {
+			long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong() - 1;
+			}
+			return res;
+		}
+
+		public long[] nextLongArray1Index(int n) {
+			long[] res = new long[n + 1];
+			for (int i = 0; i < n; i++) {
+				res[i + 1] = nextLong();
+			}
+			return res;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] res = new double[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextDouble();
+			}
+			return res;
+		}
+
+		public InputReader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			tok = new StringTokenizer("");
+		}
+	}
+
+}
