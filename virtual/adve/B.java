feaaefe9119053e7ab07275d03e1bc4eb7366403
tsuji.d <dram.dt.shonan@gmail.com>
2018-12-08 08:01:35 +0900

    auto-commit

--- virtual/adve/B.java
+++ virtual/adve/B.java
@@ -5,14 +5,35 @@ import java.util.Scanner;
 public class B {
 
 	public static void main(String[] args) {
+
+		// 標準入力からデータを受け取ります
 		Scanner in = new Scanner(System.in);
-		long x = in.nextLong(), y = in.nextLong();
-		if ((-x + 3*y) % 8 == 0 && (3*x - y) % 8 == 0 && (-x + 3*y) / 8 >= 0 && (3*x - y) / 8 >= 0) {
-			System.out.println("Yes");
-		} else {
-			System.out.println("No");
+		int n = in.nextInt(), m = in.nextInt(), d = in.nextInt();
+
+		// あみだくじに対応する配列 {0, 1, 2, 3, 4} (0-indexed) としておきます
+		int[] amida = new int[n];
+		for (int i = 0; i < n; i++) {
+			amida[i] = i;
 		}
+		for (int i = 0; i < m; i++) {
+			int a = in.nextInt()-1;
+
+			// あみだくじの横棒によって swap させます
+			int tmp = amida[a];
+			amida[a] = amida[a+1];
+			amida[a+1] = tmp;
+		}
+
+		// 解答を出力します
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[amida[i]] = i+1;
+		}
+
+		for (int i : ans) {
+			System.out.println(i);
+		}
+
 		in.close();
 	}
-
 }
