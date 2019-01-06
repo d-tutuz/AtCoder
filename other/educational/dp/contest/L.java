133d4322485b995c45530d3f1b121a5e9f45d991
tsuji.d <dram.dt.shonan@gmail.com>
2019-01-06 23:01:10 +0900

    auto-commit

---
+++ other/educational/dp/contest/L.java
@@ -0,0 +1,288 @@
+package educational.dp.contest;
+
+import static java.lang.Math.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class L {
+
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		MyInput in = new MyInput(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskX solver = new TaskX();
+		solver.solve(1, in, out);
+		out.close();
+	}
+
+	static int INF = 1 << 30;
+	static long LINF = 1L << 55;
+	static int MOD = 1000000007;
+	static int[] mh4 = { 0, -1, 1, 0 };
+	static int[] mw4 = { -1, 0, 0, 1 };
+	static int[] mh8 = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	static int[] mw8 = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	static class TaskX {
+
+		int n;
+		int[] a;
+		HashMap<T, Long> memo = new HashMap<>();
+		public void solve(int testNumber, MyInput in, PrintWriter out) {
+
+			n = in.nextInt();
+			a = in.nextIntArray(n);
+
+			long ans = f(0, a.length-1, 0);
+			out.println(ans);
+
+		}
+
+		long f(int l, int r, int state) {
+			if (memo.containsKey(new T(l, r, state))) {
+				return memo.get(new T(l, r, state));
+			}
+			if (l > r) return 0;
+			long ret = 0;
+			if (state == 0) {
+				ret = max(f(l+1, r, 1) + a[l], f(l, r-1, 1) + a[r]);
+				memo.put(new T(l, r, state), ret);
+			} else {
+				ret = min(f(l+1, r, 0) - a[l], f(l, r-1, 0) - a[r]);
+				memo.put(new T(l, r, state), ret);
+			}
+			memo.put(new T(l, r, state), ret);
+			return ret;
+		}
+	}
+
+	static class T {
+		int i, j, k;
+
+		public T(int i, int j, int k) {
+			super();
+			this.i = i;
+			this.j = j;
+			this.k = k;
+		}
+
+		@Override
+		public String toString() {
+			return "T [i=" + i + ", j=" + j + ", k=" + k + "]";
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + i;
+			result = prime * result + j;
+			result = prime * result + k;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			T other = (T) obj;
+			if (i != other.i)
+				return false;
+			if (j != other.j)
+				return false;
+			if (k != other.k)
+				return false;
+			return true;
+		}
+
+	}
+
+	static class MyInput {
+		private final BufferedReader in;
+		private static int pos;
+		private static int readLen;
+		private static final char[] buffer = new char[1024 * 8];
+		private static char[] str = new char[500 * 8 * 2];
+		private static boolean[] isDigit = new boolean[256];
+		private static boolean[] isSpace = new boolean[256];
+		private static boolean[] isLineSep = new boolean[256];
+
+		static {
+			for (int i = 0; i < 10; i++) {
+				isDigit['0' + i] = true;
+			}
+			isDigit['-'] = true;
+			isSpace[' '] = isSpace['\r'] = isSpace['\n'] = isSpace['\t'] = true;
+			isLineSep['\r'] = isLineSep['\n'] = true;
+		}
+
+		public MyInput(InputStream is) {
+			in = new BufferedReader(new InputStreamReader(is));
+		}
+
+		public int read() {
+			if (pos >= readLen) {
+				pos = 0;
+				try {
+					readLen = in.read(buffer);
+				} catch (IOException e) {
+					throw new RuntimeException();
+				}
+				if (readLen <= 0) {
+					throw new MyInput.EndOfFileRuntimeException();
+				}
+			}
+			return buffer[pos++];
+		}
+
+		public int nextInt() {
+			int len = 0;
+			str[len++] = nextChar();
+			len = reads(len, isSpace);
+			int i = 0;
+			int ret = 0;
+			if (str[0] == '-') {
+				i = 1;
+			}
+			for (; i < len; i++)
+				ret = ret * 10 + str[i] - '0';
+			if (str[0] == '-') {
+				ret = -ret;
+			}
+			return ret;
+		}
+
+		public long nextLong() {
+			int len = 0;
+			str[len++] = nextChar();
+			len = reads(len, isSpace);
+			int i = 0;
+			long ret = 0;
+			if (str[0] == '-') {
+				i = 1;
+			}
+			for (; i < len; i++)
+				ret = ret * 10 + str[i] - '0';
+			if (str[0] == '-') {
+				ret = -ret;
+			}
+			return ret;
+		}
+
+		public char nextChar() {
+			while (true) {
+				final int c = read();
+				if (!isSpace[c]) {
+					return (char) c;
+				}
+			}
+		}
+
+		public String nextString() {
+			return new String(nextChars());
+		}
+
+		public char[] nextChars() {
+			int len = 0;
+			str[len++] = nextChar();
+			len = reads(len, isSpace);
+			return Arrays.copyOf(str, len);
+		}
+
+		int reads(int len, boolean[] accept) {
+			try {
+				while (true) {
+					final int c = read();
+					if (accept[c]) {
+						break;
+					}
+					if (str.length == len) {
+						char[] rep = new char[str.length * 3 / 2];
+						System.arraycopy(str, 0, rep, 0, str.length);
+						str = rep;
+					}
+					str[len++] = (char) c;
+				}
+			} catch (MyInput.EndOfFileRuntimeException e) {
+			}
+			return len;
+		}
+
+		public int[] nextIntArray(final int n) {
+			final int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArray1Index(final int n) {
+			final int[] res = new int[n + 1];
+			for (int i = 1; i < n + 1; i++) {
+				res[i] = nextInt();
+			}
+			return res;
+		}
+
+		public int[] nextIntArrayDec(final int n) {
+			final int[] res = new int[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextInt() - 1;
+			}
+			return res;
+		}
+
+		public long[] nextLongArray(final int n) {
+			final long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArray1Index(final int n) {
+			final long[] res = new long[n + 1];
+			for (int i = 1; i < n + 1; i++) {
+				res[i] = nextLong();
+			}
+			return res;
+		}
+
+		public long[] nextLongArrayDec(final int n) {
+			final long[] res = new long[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextLong() - 1;
+			}
+			return res;
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(nextString());
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] res = new double[n];
+			for (int i = 0; i < n; i++) {
+				res[i] = nextDouble();
+			}
+			return res;
+		}
+
+		static class EndOfFileRuntimeException extends RuntimeException {
+		}
+
+	}
+
+}
